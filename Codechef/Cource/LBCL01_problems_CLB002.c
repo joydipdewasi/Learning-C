6100de9acd762dfaa16a89e0b5120d004b5bf86d
joydipdewasi <dewasijoydip@gmail.com>
2024-03-11 16:16:03 +0530

    My first time learning c

--- Codechef/Cource/LBCL01_problems_CLB002.c
+++ Codechef/Cource/LBCL01_problems_CLB002.c
@@ -4,13 +4,14 @@ Let us output an integer in C.
 C has a few lines of code which we write in almost all programs.
 These are the first 2 lines that you see in the program to your right. We are going to learn about them in some time.
 
-For now, read the Line 5: "printf("%d", 12);"*/
-
+For now, read the Line 5: "printf("%d", 12);"
+my first time learning code*/
 
 #include <stdio.h>
 
-int main() {
-  
+int main()
+{
+
   printf("%d", 12);
   return 0;
 }
