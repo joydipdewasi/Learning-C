307d898a0bd86621337ffdf6aa6ae6557fe311cf
joydipdewasi <dewasijoydip@gmail.com>
2024-03-18 23:48:32 +0530

    ..

---
+++ Codechef/Practice/output/PCL05_problems_FLOW001.c
@@ -0,0 +1,25 @@
+// Add Two Numbers
+// Your task is very simple: given two integers 
+// �
+// A and 
+// �
+// B, write a program to add these two numbers and output the sum.
+
+// Input Format
+// The first line contains an integer 
+// �
+// T, the total number of test cases.
+// Then follow 
+// �
+// T lines, each line contains two integers, 
+// �
+// A and 
+// �
+// B.
+// Output Format
+// For each test case, add 
+// �
+// A and 
+// �
+// B and display the sum in a new line.
+
